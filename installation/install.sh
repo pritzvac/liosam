0cf444f4a5b908af998ef6bdd107d463db594ecb
Pavel Petracek <petrapa6@fel.cvut.cz>
2021-01-18 11:54:20 +0100

    update install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -4,7 +4,7 @@ WORKSPACE_PATH=$GIT_PATH/workspace
 
 # install dependencies
 sudo apt update
-sudo apt-get install libtbb-dev ros-melodic-pcl-* libpcl-* ros-melodic-navigation ros-melodic-robot-localization ros-melodic-robot-state-publisher
+sudo apt-get install libtbb-dev ros-melodic-pcl-* libpcl-* ros-melodic-navigation ros-melodic-robot-localization ros-melodic-robot-state-publisher ros-melodic-imu-complementary-filter
 
 ./install_gtsam.sh
 ./install_pcl.sh
