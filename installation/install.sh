ff1b55db91f95ee011c48e1de91a412792b62ed7
Vit Kratky <kratkvit@fel.cvut.cz>
2021-02-08 15:45:58 +0100

    fix installation scripts

--- installation/install.sh
+++ installation/install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-WORKSPACE_PATH=$GIT_PATH/workspace
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+if [ -z $1 ]; then
+  WORKSPACE_PATH=$GIT_PATH/workspace
+else
+  WORKSPACE_PATH="$1"
+fi
 
 # install dependencies
 sudo apt update
 sudo apt-get install libtbb-dev ros-melodic-pcl-* libpcl-* ros-melodic-navigation ros-melodic-robot-localization ros-melodic-robot-state-publisher ros-melodic-imu-complementary-filter
 
-./install_gtsam.sh
-# ./install_pcl.sh
+$SCRIPT_PATH/install_gtsam.sh
+# $SCRIPT_PATH/install_pcl.sh
 
-cd $WORKSPACE_PATH
-catkin build
+# cd $WORKSPACE_PATH
+# catkin build
--- installation/install_gtsam.sh
+++ installation/install_gtsam.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget -O ~/tmp/gtsam.zip https://github.com/borglab/gtsam/archive/4.0.2.zip
-cd ~/tmp/ && unzip gtsam.zip -d ~/tmp/
-cd ~/tmp/gtsam-4.0.2/
+wget -O ~/Downloads/gtsam.zip https://github.com/borglab/gtsam/archive/4.0.2.zip
+cd ~/Downloads/ && unzip gtsam.zip -d ~/Downloads/
+cd ~/Downloads/gtsam-4.0.2/
 mkdir build && cd build
 cmake -DGTSAM_BUILD_WITH_MARCH_NATIVE=OFF ..
 sudo make install -j4
