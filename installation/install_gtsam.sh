c25da3144b1ef174d5d1dfe50d6704b38dd1d631
Pavel Petracek <petrapa6@fel.cvut.cz>
2021-02-11 22:27:57 +0100

    fix pcl and gtsam installation

--- installation/install_gtsam.sh
+++ installation/install_gtsam.sh
@@ -2,18 +2,34 @@
 
 if [[ $# -eq 1 ]]
 then
-  WORKSPACE_PATH=$1
+  GTSAM_PATH=$1
 else
-  WORKSPACE_PATH=$GIT_PATH/workspace
+  GTSAM_PATH=/tmp
 fi
 
-# Releases can be found at: https://github.com/borglab/gtsam
-GTSAM_VERSION=4.0.3
 
-# checkout GTSAM_VERSION (ideally stable)
-cd $GIT_PATH
-[ ! -d "gtsam" ] && git clone https://github.com/borglab/gtsam.git # clone if was not cloned before
-cd $GIT_PATH/gtsam && git checkout $GTSAM_VERSION
+if [ ! -d $GTSAM_PATH/gtsam-4.0.2 ]
+then
+  wget -O $GTSAM_PATH/gtsam.zip https://github.com/borglab/gtsam/archive/4.0.2.zip
+  cd $GTSAM_PATH && unzip gtsam.zip -d $GTSAM_PATH
+fi
 
-# link gtsam to workspace
-ln -s $GIT_PATH/gtsam $WORKSPACE_PATH/src/.
+cd $GTSAM_PATH/gtsam-4.0.2/
+
+[ ! -d "build" ] && mkdir build
+cd build
+cmake -DGTSAM_BUILD_WITH_MARCH_NATIVE=OFF ..
+sudo make install -j4
+
+# WORKSPACE_PATH=$GIT_PATH/workspace
+
+# # Releases can be found at: https://github.com/borglab/gtsam
+# GTSAM_VERSION=4.0.3
+
+# # checkout GTSAM_VERSION (ideally stable)
+# cd $GIT_PATH
+# [ ! -d "gtsam" ] && git clone https://github.com/borglab/gtsam.git # clone if was not cloned before
+# cd $GIT_PATH/gtsam && git checkout $GTSAM_VERSION
+
+# # link gtsam to workspace
+# ln -s $GIT_PATH/gtsam $WORKSPACE_PATH/src/.
