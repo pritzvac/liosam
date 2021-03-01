3db5b081dcaa16090777ecfcb68e32d05de84b70
Tomas Baca <klaxalk@gmail.com>
2021-03-01 19:47:31 +0100

    updated install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+distro=`lsb_release -r | awk '{ print $2 }'`
+[ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
+[ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
+
 # get path to script
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
@@ -12,7 +21,14 @@ fi
 
 # install dependencies
 sudo apt update
-sudo apt-get -y install libtbb-dev ros-melodic-pcl-* libpcl-* ros-melodic-navigation ros-melodic-robot-localization ros-melodic-robot-state-publisher ros-melodic-imu-complementary-filter
+sudo apt-get -y install \
+  libtbb-dev \
+  ros-$ROS_DISTRO-pcl-* \
+  libpcl-* \
+  ros-$ROS_DISTRO-navigation \
+  ros-$ROS_DISTRO-robot-localization \
+  ros-$ROS_DISTRO-robot-state-publisher \
+  ros-$ROS_DISTRO-imu-complementary-filter
 
 $SCRIPT_PATH/install_gtsam.sh $WORKSPACE_PATH
 # $SCRIPT_PATH/install_pcl.sh
