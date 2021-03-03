e6b70d55f7a0e19d610a0b06a4aa04bb02d3e305
Tomas Baca <klaxalk@gmail.com>
2021-03-03 11:37:13 +0100

    updated install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -9,14 +9,15 @@ distro=`lsb_release -r | awk '{ print $2 }'`
 [ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
 [ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
 
-# get path to script
-SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+# get the path to this script
+SCRIPT_PATH=`dirname "$0"`
+SCRIPT_PATH=`( cd "$MY_PATH" && pwd )`
 
 if [[ $# -eq 1 ]]
 then
   WORKSPACE_PATH=$1
 else
-  WORKSPACE_PATH=$GIT_PATH/workspace
+  WORKSPACE_PATH=$HOME/workspace
 fi
 
 # install dependencies
