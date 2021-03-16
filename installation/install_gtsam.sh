4ba5d79d1d31e1993e5f2c2af7da04047dd4e88b
Pavel Petracek <petrapa6@fel.cvut.cz>
2021-03-16 12:40:18 +0100

    improve installation process

--- installation/install_gtsam.sh
+++ installation/install_gtsam.sh
@@ -8,21 +8,20 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 distro=`lsb_release -r | awk '{ print $2 }'`
 [ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
 [ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
-
-if [[ $# -eq 1 ]]
-then
-  GTSAM_PATH=$1
-else
-  GTSAM_PATH=/tmp
-fi
-
 [ "$distro" = "18.04" ] && GTSAM_VERSION=4.0.2
 [ "$distro" = "20.04" ] && GTSAM_VERSION=4.0.3
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+GTSAM_PATH=$SCRIPT_PATH/../lib
+
+[ ! -d $GTSAM_PATH ] && mkdir -p $GTSAM_PATH
+cd $GTSAM_PATH
 
 if [ ! -d $GTSAM_PATH/gtsam-$GTSAM_VERSION ]
 then
   wget -O $GTSAM_PATH/gtsam.zip https://github.com/borglab/gtsam/archive/$GTSAM_VERSION.zip
   cd $GTSAM_PATH && unzip gtsam.zip -d $GTSAM_PATH
+  rm -f gtsam.zip
 fi
 
 cd $GTSAM_PATH/gtsam-$GTSAM_VERSION/
@@ -30,7 +29,7 @@ cd $GTSAM_PATH/gtsam-$GTSAM_VERSION/
 [ ! -d "build" ] && mkdir build
 cd build
 cmake -DGTSAM_BUILD_WITH_MARCH_NATIVE=OFF ..
-make -j4
+make -j$[$(nproc) - 1]
 sudo make install
 
 # WORKSPACE_PATH=$GIT_PATH/workspace
